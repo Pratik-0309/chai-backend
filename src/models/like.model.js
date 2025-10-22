f2bc79143241147a72bb634171dcbd871ef4239d
PratikParate <paratepratik10@gmail.com>
2025-10-23 03:34:40 +0530

    add models for comments, likes, playlists, and tweets

---
+++ src/models/like.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+  {
+    likedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+      required: true,
+    },
+    tweet:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tweet",
+    },
+    comment:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "comment",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Like = mongoose.model("Like", likeSchema);
